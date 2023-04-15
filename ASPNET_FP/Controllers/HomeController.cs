54108d5a1673344fffbf6e9aa83c0ae18a5a23ad
Alvaro Beltran Caceres <n01511461@humbermail.ca>
2023-04-15 17:06:56 -0400

    added notifications draft

--- ASPNET_FP/Controllers/HomeController.cs
+++ ASPNET_FP/Controllers/HomeController.cs
@@ -1,92 +1,1 @@
-<<<<<<< HEAD
-﻿using ASPNET_FP.Data;
-using ASPNET_FP.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-=======
-﻿using ASPNET_FP.Data;
-using ASPNET_FP.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
->>>>>>> 86fef26f3cb672ccb92a9b900a74f7f0549a018e
-using System.Diagnostics;
-
-namespace ASPNET_FP.Controllers
-{
-	public class HomeController : Controller
-	{
-<<<<<<< HEAD
-		private readonly MyLesseeDBContext context;
-
-		public HomeController(MyLesseeDBContext myLesseeDBContext)
-		{
-			this.context = myLesseeDBContext;
-		}
-
-		// show all listings with filter on houseType and rentalPrice
-		public IActionResult Index(string houseType, string rentalPrice)
-		{
-			var listings = context.Listings
-				.Include(l => l.HouseType)
-				.OrderBy(l => l.ListingId)
-				.ToList();
-
-			if (!string.IsNullOrEmpty(houseType))
-			{
-				listings = listings.Where(l => l.HouseType.Type == houseType).ToList();
-			}
-
-			if (!string.IsNullOrEmpty(rentalPrice))
-			{
-				double price = double.Parse(rentalPrice);
-				listings = listings.Where(l => l.RentalPrice >= price).ToList();
-			}
-
-			ViewBag.HouseTypes = context.HouseTypes.OrderBy(ht => ht.Type).ToList();
-
-			return View(listings);
-=======
-		private readonly MyLesseeDBContext context;
-
-		public HomeController(MyLesseeDBContext myLesseeDBContext)
-		{
-			this.context = myLesseeDBContext;
-		}
-
-		// show all listings with filter on houseType and rentalPrice
-		public IActionResult Index(string houseType, string rentalPrice)
-		{
-			var listings = context.Listings
-				.Include(l => l.HouseType)
-				.OrderBy(l => l.ListingId)
-				.ToList();
-
-			if (!string.IsNullOrEmpty(houseType))
-			{
-				listings = listings.Where(l => l.HouseType.Type == houseType).ToList();
-			}
-
-			if (!string.IsNullOrEmpty(rentalPrice))
-			{
-				double price = double.Parse(rentalPrice);
-				listings = listings.Where(l => l.RentalPrice >= price).ToList();
-			}
-
-			ViewBag.HouseTypes = context.HouseTypes.OrderBy(ht => ht.Type).ToList();
-
-			return View(listings);
->>>>>>> 86fef26f3cb672ccb92a9b900a74f7f0549a018e
-		}
-
-		public IActionResult Privacy()
-		{
-			return View();
-		}
-
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
-}
+using ASPNET_FP.Models;using Microsoft.AspNetCore.Mvc;using Microsoft.EntityFrameworkCore;using ASPNET_FP.Data;using System.Diagnostics;namespace ASPNET_FP.Controllers{	public class HomeController : Controller	{		private readonly MyLesseeDBContext context;		public HomeController(MyLesseeDBContext myLesseeDBContext)		{			this.context = myLesseeDBContext;		}		// show all listings with filter on houseType and rentalPrice		public IActionResult Index(string houseType, string rentalPrice)		{			var listings = context.Listings				.Include(l => l.HouseType)				.OrderBy(l => l.ListingId)				.ToList();			if (!string.IsNullOrEmpty(houseType))			{				listings = listings.Where(l => l.HouseType.Type == houseType).ToList();			}			if (!string.IsNullOrEmpty(rentalPrice))			{				double price = double.Parse(rentalPrice);				listings = listings.Where(l => l.RentalPrice >= price).ToList();			}			ViewBag.HouseTypes = context.HouseTypes.OrderBy(ht => ht.Type).ToList();			return View(listings);		}		public IActionResult Privacy()		{			return View();		}		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]		public IActionResult Error()		{			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });		}	}}
