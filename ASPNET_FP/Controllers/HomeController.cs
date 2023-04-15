629beec6559f04bddb7bcf80da597eb73cc2cf7c
Alvaro Beltran Caceres <n01511461@humbermail.ca>
2023-04-15 16:59:16 -0400

    added notifications and dummy data

--- ASPNET_FP/Controllers/HomeController.cs
+++ ASPNET_FP/Controllers/HomeController.cs
@@ -1,21 +1,42 @@
-﻿using ASPNET_FP.Models;
+﻿using ASPNET_FP.Data;
+using ASPNET_FP.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace ASPNET_FP.Controllers
 {
 	public class HomeController : Controller
 	{
-		private readonly ILogger<HomeController> _logger;
+		private readonly MyLesseeDBContext context;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(MyLesseeDBContext myLesseeDBContext)
 		{
-			_logger = logger;
+			this.context = myLesseeDBContext;
 		}
 
-		public IActionResult Index()
+		// show all listings with filter on houseType and rentalPrice
+		public IActionResult Index(string houseType, string rentalPrice)
 		{
-			return View();
+			var listings = context.Listings
+				.Include(l => l.HouseType)
+				.OrderBy(l => l.ListingId)
+				.ToList();
+
+			if (!string.IsNullOrEmpty(houseType))
+			{
+				listings = listings.Where(l => l.HouseType.Type == houseType).ToList();
+			}
+
+			if (!string.IsNullOrEmpty(rentalPrice))
+			{
+				double price = double.Parse(rentalPrice);
+				listings = listings.Where(l => l.RentalPrice >= price).ToList();
+			}
+
+			ViewBag.HouseTypes = context.HouseTypes.OrderBy(ht => ht.Type).ToList();
+
+			return View(listings);
 		}
 
 		public IActionResult Privacy()
--- ASPNET_FP/Controllers/ListingController.cs
+++ ASPNET_FP/Controllers/ListingController.cs
@@ -1,1 +1,30 @@
-﻿using ASPNET_FP.Data;using ASPNET_FP.Models;using Microsoft.AspNetCore.Mvc;using Microsoft.EntityFrameworkCore;namespace ASPNET_FP.Controllers{	public class ListingController : Controller	{		private readonly MyLesseeDBContext context;		public ListingController(MyLesseeDBContext myLesseeDBContext)		{			this.context = myLesseeDBContext;		}        public IActionResult Index()        {            var listings = context.Listings                .Include(l => l.HouseType)                .OrderBy(l => l.HouseType)                .ToList();            return View(listings);        }        [HttpGet]        public IActionResult Details(int id)        {            var l = context.Listings.Find(id);            return View(l);        }        [HttpGet]        public IActionResult Add()        {            ViewBag.Action = "Add";            ViewBag.HouseType = context.HouseTypes.OrderBy(ht => ht.Type).ToList();            return View("Edit", new Listing());        }        [HttpGet]        public IActionResult Edit(int id)        {            ViewBag.Action = "Edit";            ViewBag.HouseType = context.HouseTypes.OrderBy(ht => ht.Type).ToList();            var l = context.Listings.Find(id);            return View(l);        }        [HttpPost]        public IActionResult Edit(Listing l)        {            if (ModelState.IsValid)            {                if (l.ListingId == 0)                    context.Listings.Add(l);                else                    context.Listings.Update(l);                context.SaveChanges();                return RedirectToAction("Index", "Listing");            }            else            {                ViewBag.Action = (l.ListingId == 0) ? "Add" : "Edit";                ViewBag.Genres = context.HouseTypes.OrderBy(ht => ht.Type).ToList();                return View(l);            }        }        [HttpGet]        public IActionResult Delete(int id)        {            var l = context.Listings.Find(id);            return View(l);        }        [HttpPost]        public IActionResult Delete(Listing l)        {            context.Listings.Remove(l);            context.SaveChanges();            return RedirectToAction("Index", "Listing");        }    }}
+using ASPNET_FP.Data;using ASPNET_FP.Models;using Microsoft.AspNetCore.Mvc;using Microsoft.EntityFrameworkCore;namespace ASPNET_FP.Controllers{
+    public class ListingController : Controller
+    {
+        private readonly MyLesseeDBContext context;
+
+        public ListingController(MyLesseeDBContext myLesseeDBContext)
+        {
+            this.context = myLesseeDBContext;
+        }        // show all listings        public IActionResult Index()        {            var listings = context.Listings                .Include(l => l.HouseType)                .OrderBy(l => l.ListingId)                .ToList();            return View(listings);        }        /*// show all listings with filter on houseType and rentalPrice        public IActionResult Index(string houseType, string rentalPrice)
+        {
+            var listings = context.Listings
+                .Include(l => l.HouseType)
+                .OrderBy(l => l.ListingId)
+                .ToList();
+
+            if (!string.IsNullOrEmpty(houseType))
+            {
+                listings = listings.Where(l => l.HouseType.Type == houseType).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(rentalPrice))
+            {
+                double price = double.Parse(rentalPrice);
+                listings = listings.Where(l => l.RentalPrice >= price).ToList();
+            }
+
+            ViewBag.HouseTypes = context.HouseTypes.OrderBy(ht => ht.Type).ToList();
+
+            return View(listings);
+        }*/        [HttpGet]        public IActionResult Details(int id)        {            var l = context.Listings.Find(id);            return View(l);        }        [HttpGet]        public IActionResult Add()        {            ViewBag.Action = "Add";            ViewBag.HouseTypes = context.HouseTypes.OrderBy(ht => ht.Type).ToList();            return View("Edit", new Listing());        }        [HttpGet]        public IActionResult Edit(int id)        {            ViewBag.Action = "Edit";            ViewBag.HouseTypes = context.HouseTypes.OrderBy(ht => ht.Type).ToList();            var l = context.Listings.Find(id);            return View(l);        }        [HttpPost]        public IActionResult Edit(Listing l)        {            if (ModelState.IsValid)            {                if (l.ListingId == 0)                {                    context.Listings.Add(l);                }                else                {                    context.Listings.Update(l);                }                context.SaveChanges();                return RedirectToAction("Index", "Listing");            }            else            {                ViewBag.Action = (l.ListingId == 0) ? "Add" : "Edit";                ViewBag.HouseTypes = context.HouseTypes.OrderBy(ht => ht.Type).ToList();                return View(l);            }        }        [HttpGet]        public IActionResult Delete(int id)        {            var l = context.Listings.Find(id);            return View(l);        }        [HttpPost]        public IActionResult Delete(Listing l)        {            context.Listings.Remove(l);            context.SaveChanges();            return RedirectToAction("Index", "Listing");        }    }}
